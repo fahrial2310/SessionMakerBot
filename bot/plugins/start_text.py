88bb4ea96127efa28457143d8da30f2c9ba5db0a
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2022-05-25 09:29:27 +0800

    Update start_text.py

--- bot/plugins/start_text.py
+++ bot/plugins/start_text.py
@@ -23,14 +23,6 @@ from bot import (
     SUPPORT_GRP,
     SUPPORT_MODE      
 )
-from bot.__init__ import Var, TXT
-
-Bot = Client(
-      "SessionMakerBot"
-      api_hash=Var.API_HASH, 
-      api_id=Var.APP_ID, 
-      bot_token=Var.TG_BOT_TOKEN,
-)
 
 @Client.on_message(
     filters.command(START_COMMAND, COMMM_AND_PRE_FIX) &
@@ -44,15 +36,6 @@ async def num_start_message(_, message: Message):
     AKTIFPERINTAH[message.chat.id]["START"] = status_message
     raise message.stop_propagation()
 
-@Bot.on_callback_query()
-async def cdata(c, q):
-    chat_id = q.from_user.id 
-    data = q.data
-    if data == "help":
-    await q.answer(wait)
-    await q.message.edit_text( text=Tr.START_OTHER_USER_TEXT + "\n" + INPUT_PHONE_NUMBER,  
-    disable_web_page_preview=True )
-
 @Client.on_message(filters.private & filters.command('start'))
 def _start(client, message):
     chatID = message.chat.id
@@ -64,7 +47,6 @@ def _start(client, message):
     reply_markup = InlineKeyboardMarkup(
                     [[InlineKeyboardButton(f"{emo} {CREATOR_MODE}", url=f"t.me/{CREATOR_USERNAME}")],
                     [InlineKeyboardButton(f"{emoj} {UPDATES_MODE}", url=f"t.me/{UPDATES_CH}"),
-                     InlineKeyboardButton(" Help ", callback_data ="help"),
                      InlineKeyboardButton(f"{em} {SUPPORT_MODE}", url=f"t.me/{SUPPORT_GRP}")],
                     [InlineKeyboardButton("☠️ Repo PyrogramBot ☠️", url="https://github.com/fahrial2310/PyrogramSessionMaker")
                     ]]
